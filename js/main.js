c7d91fccd506538cfc741b05c49ca8e9162fa501
Theron Patrick <theronbpatrick@gmail.com>
2015-12-30 17:09:52 -0500

    Get dat truffle shuffle

--- js/main.js
+++ js/main.js
@@ -1,21 +1,98 @@
 $(document).ready(function() {
-    console.log("sup");
+    console.log("sup1");
 
-    $(".minus").click(function() {
-        var amount = $(this).parent().find("input");
-        var value = amount.val();
-        value = parseInt(value) - 10;
-        amount.val(value);
-    })
+    function init() {
+        initFastClick();
+        initCards();
+        attachHandlers();
+    }
 
-    $(".plus").click(function() {
-        var amount = $(this).parent().find("input");
-        var value = amount.val();
-        value = parseInt(value) + 10;
-        amount.val(value);
-    })
+    function initFastClick() {
+        $(function() {
+            FastClick.attach(document.body);
+        });
+    }
 
-    $("input").submit(function(e) {
-        e.preventDefault();
-    })
-})
+    var deck = [];
+    function initCards() {
+        // Get cards
+        var originalCards;
+
+        var url = "https://spreadsheets.google.com/feeds/list/13EV2ev7_2oIur5C0dq-uo-4w9-jCSvDRKhxNO5gBr9k/od6/public/values?alt=json";
+
+        $.get(url)
+        .then(function(response) {
+            cards = response.feed.entry;
+
+            $.each(cards, function() {
+                deck.push({
+                    name: this["gsx$name"]["$t"],
+                    description: this["gsx$description"]["$t"]
+                });
+            });
+
+            console.log("cards are" , deck);
+
+            shuffle(deck);
+            drawThreeCards();
+
+
+        });
+    }
+
+    function drawThreeCards() {
+        for (var i = 0; i<3; i++) {
+
+            var domCards = $(".card");
+
+            if (deck.length == 0) {
+                $(domCards[i]).find("h2").text("Out of cards");
+                $(domCards[i]).find("p").text("");
+                continue;
+            }
+
+            var card = deck.pop();
+
+            $(domCards[i]).find("h2").text(card.name);
+            $(domCards[i]).find("p").text(card.description);
+
+        }
+
+        console.log("deck now is " , deck);
+    }
+
+    function shuffle(o){
+        for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+    }
+
+    function attachHandlers() {
+        $(".minus").click(function() {
+            var amount = $(this).parent().find("input");
+            var value = amount.val();
+            value = parseInt(value) - 10;
+            amount.val(value);
+        })
+
+        $(".plus").click(function() {
+            var amount = $(this).parent().find("input");
+            var value = amount.val();
+            value = parseInt(value) + 10;
+            amount.val(value);
+        })
+
+        $("input").submit(function(e) {
+            e.preventDefault();
+        })
+
+        $(".draw-cards").click(function() {
+            drawThreeCards();
+        })
+
+        $(".new-deck").click(function() {
+            initCards();
+        })
+    }
+
+    init();
+});
